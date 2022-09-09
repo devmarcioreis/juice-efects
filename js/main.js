dae2cec2664f074543d6fc787b943c646cc63b9d
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-09-09 13:59:16 -0300

    Add project

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*=============== GSAP ===============*/
+TweenMax.from('.home__title', 1, {delay: .2, opacity: 0, y: 20, ease: Expo.easeInOut})
+
+TweenMax.from('.home__description', 1, {delay: .4, opacity: 0, y: 20, ease: Expo.easeInOut})
+
+TweenMax.from('.home__button', 1, {delay: .7, opacity: 0, y: 20, ease: Expo.easeInOut})
+
+TweenMax.from('.home__liquid', 1, {delay: .2, opacity: 0, y: 200, ease: Expo.easeInOut})
+
+TweenMax.from('.home__juice-animate', 1, {delay: .2, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('.home__apple1', 1, {delay: 0.7, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('.home__apple2', 1, {delay: .5, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('#left1', 2, {delay: .6, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('#left2', 2, {delay: .7, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('#left3', 2, {delay: .8, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('#left4', 2, {delay: .1, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('#left5', 2, {delay: .2, opacity: 0, y: -800, ease: Expo.easeInOut})
+
+TweenMax.from('#left6', 2, {delay: .4, opacity: 0, y: -800, ease: Expo.easeInOut})
